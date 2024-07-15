17211a48812d593dacc6d5d7281902414d96c88e
Simone Parente <parentesimone.01@gmail.com>
2024-07-15 13:46:01 +0200

    navbar v1.0

--- HiveMind-Frontend/src/app/app.routes.ts
+++ HiveMind-Frontend/src/app/app.routes.ts
@@ -2,8 +2,15 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from './login/login.component'
 import { RegisterComponent } from '../register/register.component';
 import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 export const routes: Routes = [
+    {
+        path: "",
+        title: "Prova",
+        component: AppComponent,
+    },
     {
         path: "login",
         title: "Login",
@@ -18,5 +25,10 @@ export const routes: Routes = [
         path: "home",
         title: "Home",
         component: HomeComponent
+    },
+    {
+        path: "navbar",
+        title: "navbar",
+        component: NavbarComponent
     }
 ];
