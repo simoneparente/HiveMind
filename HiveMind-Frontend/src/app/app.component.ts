1dbea3a22fd5270e5f58f09833b1735a400d33fe
Simone Parente <parentesimone.01@gmail.com>
2024-07-15 13:45:55 +0200

    added bootstrap + zone.js

--- HiveMind-Frontend/src/app/app.component.ts
+++ HiveMind-Frontend/src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbModal, NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-app';
+  constructor(private modalService: NgbModal) {
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
+
 }
