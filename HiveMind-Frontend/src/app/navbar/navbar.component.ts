d11e8c0265dbc073df59ec6b692684515b58d01f
Simone Parente <parentesimone.01@gmail.com>
2024-07-17 12:34:18 +0200

    navbar v1.1

--- HiveMind-Frontend/src/app/navbar/navbar.component.ts
+++ HiveMind-Frontend/src/app/navbar/navbar.component.ts
@@ -1,20 +1,18 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  standalone: false,
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  logo= "assets/HiveMindLogo.png";
-
-
+  logo = "assets/HiveMindLogo.png";
+  loginPath = "/login";
+  unpopularPath = "/unpopular";
+  mainstreamPath = "/mainstream";
   constructor(private router: Router){}
-  
-  navigateToHome(){
-    this.router.navigate(['/login']);
-
-  }
 }
