a83f724b74fda3a3bd0c8685c4c336bc567f3bfd
Simone Parente <parentesimone.01@gmail.com>
2025-02-28 11:13:02 +0100

    Folder refactoring and old versions deletion

--- HiveMind_old2/src/routes/CommentRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express  from "express";
-import CommentController from "../controller/CommentController";
-
-const commentRouter = new express.Router();
-
-commentRouter.post("/post/:ideaID", CommentController.postComment);
-
-export default commentRouter;
