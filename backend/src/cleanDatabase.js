aa7e1dcf89badc340adfe8dc40e5ebd9cf3dbd62
Simone Parente <parentesimone.01@gmail.com>
2025-03-07 15:54:41 +0100

    fix: fixed cleanup not working

--- backend/src/cleanDatabase.js
+++ backend/src/cleanDatabase.js
@@ -1,10 +1,29 @@
 import sequelize from "./data/db.js";
+import User from "./models/User.js";
+import Idea from "./models/Idea.js";
+import Vote from "./models/Vote.js";
+import Comment from "./models/Comment.js";
+import { createAssociations } from "./models/associations.js";
 
 async function cleanDatabase() {
     try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+
+        await sequelize.query('CREATE SCHEMA IF NOT EXISTS h;');
+
+        User.createModel();
+        Idea.createModel();
+        Vote.createModel();
+        Comment.createModel();
+
+        createAssociations();
+
+        await sequelize.drop({ schema: 'h' });
+
         await sequelize.sync({ force: true });
+
         console.log("Database cleaned successfully");
-        process.exit(0);
     } catch (error) {
         console.error("Error cleaning database:", error);
         process.exit(1);
@@ -12,4 +31,3 @@ async function cleanDatabase() {
 }
 
 await cleanDatabase();
-
